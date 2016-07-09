0695dfe43cab440453519763c9d62a472663d3f4
Jasmine Hurst <jasmine.ehurst@gmail.com>
2016-07-09 00:55:24 -0500

    Initial commit

---
+++ PairInArrayMatchingSum/ArrayPair.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void findPairs(int array[], int sum, int size);
+
+int main(){
+	 int array[] = {1, 4, 45, 6, 10, -8};
+	 int sum = 16;
+	 int size = 6;
+	 
+	 findPairs(array, sum, size);
+}
+
+void findPairs(int array[], int sum, int size){
+	int array_size = size;
+	for(int i = 0; i < array_size; i++){
+		for(int j = i + 1;j < array_size; j++){
+			if(array[i] + array[j] == sum){
+				printf("There is a pair in the array ");
+				for(int n = 0; n < array_size; n++){
+					printf("%d ", array[n]);
+				}
+				printf("that creates the sum %d ", sum);
+			}
+	
+
+		}
+	}
+}
